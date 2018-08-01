37fb84eb9058a681ecd7d1efcde365a2da6a5bd9
lijianchang <liji.anchang@163.com>
2018-08-01 17:03:20 +0800

    更新ResourceUtils

--- base-lib/src/main/java/com/ryan/baselib/util/ResourceUtils.java
+++ base-lib/src/main/java/com/ryan/baselib/util/ResourceUtils.java
@@ -1,6 +1,8 @@
 package com.ryan.baselib.util;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 
@@ -12,5 +14,29 @@ public class ResourceUtils {
 
     public static Drawable getDrawable(Context context, int resId) {
         return ContextCompat.getDrawable(context, resId);
+    }
+
+    public static String getString(Context context, int resId) {
+        return context.getResources().getString(resId);
+    }
+
+    public static String getString(Context context, int resId, Object... formatArgs) {
+        return context.getResources().getString(resId, formatArgs);
+    }
+
+    public static TypedArray obtainTypedArray(Context context, int arrayId) {
+        return context.getResources().obtainTypedArray(arrayId);
+    }
+
+    public static String[] getStringArray(Context context, int arrayId) {
+        return context.getResources().getStringArray(arrayId);
+    }
+
+    public static int[] getIntegerArray(Context context, int arrayId) {
+        return context.getResources().getIntArray(arrayId);
+    }
+
+    public static AssetManager getAsset(Context context) {
+        return context.getResources().getAssets();
     }
 }
