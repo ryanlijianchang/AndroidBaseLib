bee01f54f17a0cd1cfecae87a1801a30ed8e6a87
lijianchang <liji.anchang@163.com>
2018-10-25 16:00:01 +0800

    增加判断手机系统：小米、华为、魅族

--- base-lib/src/main/java/com/ryan/baselib/util/DeviceUtils.java
+++ base-lib/src/main/java/com/ryan/baselib/util/DeviceUtils.java
@@ -4,17 +4,42 @@ import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Build;
+import android.os.Environment;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Properties;
 
 public class DeviceUtils {
+    private static final String KEY_MIUI_VERSION_CODE = "ro.miui.ui.version.code";
+    private static final String KEY_MIUI_VERSION_NAME = "ro.miui.ui.version.name";
+    private static final String KEY_MIUI_INTERNAL_STORAGE = "ro.miui.internal.storage";
+    private static final String KEY_EMUI_API_LEVEL = "ro.build.hw_emui_api_level";
+    private static final String KEY_EMUI_VERSION = "ro.build.version.emui";
+    private static final String KEY_EMUI_CONFIG_HW_SYS_VERSION = "ro.confg.hw_systemversion";
+
+    /**
+     * 华为系统
+     */
+    public static final String SYS_EMUI = "sys_emui";
+    /**
+     * 小米系统
+     */
+    public static final String SYS_MIUI = "sys_miui";
+    /**
+     * 魅族系统
+     */
+    public static final String SYS_FLYME = "sys_flyme";
+
     /**
      * 总内存，MB
      */
     private static int sTotalMem = 0;
 
     private static boolean mIsMemInfoInit = false;
+
 
     /**
      * 获得总内存大小
@@ -46,7 +71,45 @@ public class DeviceUtils {
         return sTotalMem;
     }
 
-    private static int parseFileForValue(String textToMatch, FileInputStream stream) {
+    public static String getSystem() {
+        String sys = "";
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(new File(Environment.getRootDirectory(), "build.prop")));
+            if (properties.getProperty(KEY_MIUI_VERSION_CODE, null) != null
+                    || properties.getProperty(KEY_MIUI_VERSION_NAME, null) != null
+                    || properties.getProperty(KEY_MIUI_INTERNAL_STORAGE, null) != null) {
+                sys = SYS_MIUI;
+            } else if (properties.getProperty(KEY_EMUI_API_LEVEL, null) != null
+                    || properties.getProperty(KEY_EMUI_VERSION, null) != null
+                    || properties.getProperty(KEY_EMUI_CONFIG_HW_SYS_VERSION, null) != null
+                    ) {
+                sys = SYS_EMUI;
+            } else if (getMeizuFlymeOSFlag().toLowerCase().contains("flyme")) {
+                sys = SYS_FLYME;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return sys;
+        }
+        return sys;
+    }
+
+    private static String getMeizuFlymeOSFlag() {
+        String defaultValue = "";
+        try {
+            Class<?> clz = Class.forName("android.os.SystemProperties");
+            Method get = clz.getMethod("get", String.class, String.class);
+            return (String) get.invoke(clz, "ro.build.display.id", defaultValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return defaultValue;
+    }
+
+
+    private static int parseFileForValue(String text, FileInputStream stream) {
         byte[] buffer = new byte[1024];
         try {
             int length = stream.read(buffer);
@@ -58,11 +121,11 @@ public class DeviceUtils {
                     for (int j = i; j < length; j++) {
                         int textIndex = j - i;
                         //Text doesn't match query at some point.
-                        if (buffer[j] != textToMatch.charAt(textIndex)) {
+                        if (buffer[j] != text.charAt(textIndex)) {
                             break;
                         }
                         //Text matches query here.
-                        if (textIndex == textToMatch.length() - 1) {
+                        if (textIndex == text.length() - 1) {
                             return extractValue(buffer, j);
                         }
                     }
